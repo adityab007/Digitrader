863276427e4425c86e5fd14d01a9f4ab94bcccc3
shivpanks19 <31560490+shivpanks19@users.noreply.github.com>
2018-03-29 14:23:49 +0530

    initial 2

---
+++ app/src/main/java/com/example/dell/anew/Delete.java
@@ -0,0 +1,16 @@
+package com.example.dell.anew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.dell.Digitrader.R;
+
+public class Delete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete);
+
+    }
+}
